241a261287a91abe5d0bdd9ff905253aa77c81cb
oyesanyf <oyesanyf@gmail.com>
2026-09-25 20:58:30 -0500

    build(release): rebuild signed release binary, re-sign configs, and compile updated MSI installer

--- deploy/install.ps1
+++ deploy/install.ps1
@@ -1,4 +1,4 @@
-# OpenỌ̀ṣọ́ọ̀sì Target-Side Installation Helper
+﻿# Opená»ŒÌ€á¹£á»Ìá»Ì€sÃ¬ Target-Side Installation Helper
 # 1. Install/Update Sysmon
 if (Test-Path "Sysmon64.exe") {
     echo "Installing Sysmon with security configuration..."
